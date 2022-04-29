cbe5371d52f2d1ba8eeaa0ff91387a07bf116c0a
Bruzaum <62675727+Bruzaum@users.noreply.github.com>
2022-04-29 20:52:11 -0300

    Add files via upload

---
+++ OlaMetodo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unesp.rc.hello;
+
+/**
+ *
+ * @author aluno
+ */
+public class OlaMetodo {
+    
+    static void imprimir(String palavra) {
+        System.out.print("Olá, " + palavra + "!");
+    }
+    
+    public static void main(String[] args){
+        imprimir("mundo");
+    }
+}
